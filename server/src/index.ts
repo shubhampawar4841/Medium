84f7bc4db0fff539ac26d08514071ab155d38cdc
shubhampawar4841 <your-email@example.com>
2024-09-25 22:01:03 +0530

    prisma integration

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,9 +1,35 @@
-import { Hono } from 'hono'
+import { Hono } from 'hono';
+import { PrismaClient } from '@prisma/client/edge'
+import { withAccelerate } from '@prisma/extension-accelerate'
+// Create the main Hono app
+const app = new Hono();
 
-const app = new Hono()
+app.post('/api/v1/signup', (c) => {
+  const prisma = new PrismaClient({
+    datasourceUrl: env.DATABASE_URL,
+}).$extends(withAccelerate())
 
-app.get('/', (c) => {
-  return c.text('Hello Hono!')
+
+	return c.text('signup route')
 })
 
-export default app
+app.post('/api/v1/signin', (c) => {
+	return c.text('signin route')
+})
+
+app.get('/api/v1/blog/:id', (c) => {
+	const id = c.req.param('id')
+	console.log(id);
+	return c.text('get blog route')
+})
+
+app.post('/api/v1/blog', (c) => {
+
+	return c.text('signin route')
+})
+
+app.put('/api/v1/blog', (c) => {
+	return c.text('signin route')
+})
+
+export default app;
