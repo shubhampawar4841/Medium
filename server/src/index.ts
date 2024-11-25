bbc9a9ccff4fa3fdd5dfa446850a8b0d8cdacc3f
shubhampawar4841 <shubhampawar4036@gmail.com>
2024-11-25 12:43:08 +0530

    err

--- server/src/index.ts
+++ server/src/index.ts
@@ -1,25 +1,21 @@
-import { Hono } from "hono";
-import { userRouter } from "./routes/user";
-import { blogRouter } from "./routes/blog";
-import { cors } from "hono/cors";
-
+import { Hono } from 'hono'
+import { userRouter } from './routes/user'
+import { blogRouter } from './routes/blog'
+import { cors } from 'hono/cors'
 // Create the main Hono app
 const app = new Hono<{
 	Bindings: {
-		DATABASE_URL: string;
-		JWT_SECRET: string;
-	};
+		DATABASE_URL: string,
+		JWT_SECRET: string,
+	}
 }>();
 
-// Apply CORS middleware to all routes
-app.use("*", cors({ 
-  origin: "*", // Allow all origins; modify as per your security requirements
-  allowMethods: ["GET", "POST", "PUT", "DELETE"], // Restrict allowed methods
-  allowHeaders: ["Authorization", "Content-Type"], // Allow custom headers
-}));
+app.use('/*', cors())
+app.route("api/v1/user", userRouter)
+app.route("api/v1/blog", blogRouter)
 
+app.use('/message/*', async (c, next) => {
+  await next()
+})
 
-app.route("/api/v1/user", userRouter);
-app.route("/api/v1/blog", blogRouter);
-
-export default app;
+export default app
