636d7b5651a6318ad83467c3c3998fce23bb44e8
shubhampawar4841 <shubhampawar4036@gmail.com>
2024-12-12 17:02:39 +0530

    tsconfig

--- client/tsconfig.tsx
+++ client/tsconfig.tsx
@@ -1,1 +1,25 @@
-export const BACKEND_URL = "https://server.shubhampawar4036.workers.dev"
+{
+	"compilerOptions": {
+		"target": "ES2020",
+		"useDefineForClassFields": true,
+		"lib": ["ES2020", "DOM", "DOM.Iterable"],
+		"module": "ESNext",
+		"skipLibCheck": true,
+
+		/* Bundler mode */
+		"moduleResolution": "bundler",
+		"allowImportingTsExtensions": true,
+		"resolveJsonModule": true,
+		"isolatedModules": true,
+		"noEmit": true,
+		"jsx": "react-jsx",
+
+		/* Linting */
+		"strict": true,
+		"noUnusedLocals": true,
+		"noUnusedParameters": true,
+		"noFallthroughCasesInSwitch": true
+	},
+	"include": ["src", "config.ts"],
+	"references": [{ "path": "./tsconfig.node.json" }]
+}
